c7e7599ab2dab698816c7ccc37810f5c325ee64a
VishalChoudhary <vishalghn953@gmail.com>
2023-05-16 01:22:42 +0530

    navbar commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Announcement />
+            <Navbar />
+        </div>
     )
 }
 
